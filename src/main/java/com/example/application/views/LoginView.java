f6ebdf38ae866212c3e59bc95fac3bfbe5caae9d
Musab Shaikh <musabs2802@gmail.com>
2022-06-07 18:49:27 +0530

    fix Login page error view

--- src/main/java/com/example/application/views/LoginView.java
+++ src/main/java/com/example/application/views/LoginView.java
@@ -4,13 +4,13 @@ import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.login.LoginForm;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.BeforeEnterEvent;
-import com.vaadin.flow.router.BeforeEnterListener;
+import com.vaadin.flow.router.BeforeEnterObserver;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
 @PageTitle("Login | Vaadin CRM")
 @Route("login")
-public class LoginView extends VerticalLayout implements BeforeEnterListener {
+public class LoginView extends VerticalLayout implements BeforeEnterObserver {
 
     private LoginForm loginForm = new LoginForm();
 
